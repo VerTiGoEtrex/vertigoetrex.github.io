fc8f487780c29ceb67fd6e56c608266f7764c354
Noah Crocker <necrocke@umich.edu>
2015-08-03 22:24:06 -0700

    This commit message will probably be around forever, because there's always one or two files that never get touched after the first commit.

---
+++ js/historyify.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function() {
+  var siteUrl = 'http://'+(document.location.hostname||document.location.host);
+
+  //  Catch all internally-focused links and push a new state.
+  //  Note: External links will not be affected by this behavior.
+  $(document).delegate('a[href^="/"],a[href^="'+siteUrl+'"]', "click", function(e) {
+    e.preventDefault();
+    History.pushState({}, "", this.pathname);
+  });
+
+  History.Adapter.bind(window, 'statechange', function(){
+    var State = History.getState();
+    $.get(State.url, function(data){
+      var title = data.match(/<title>(.*?)<\/title>/)[1];
+      document.title = title;
+      $('.page-content').html($(data).filter('.page-content').children(":first"));
+      ga('send', 'pageview', {
+        page: State.url,
+        title: title
+      });
+    });
+  });
+});
